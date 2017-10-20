d45f3f2c7a17f962f4bee441c723b17245b6dbdd
Myles Watson <mylesgw@google.com>
2017-10-20 16:15:54 +0530

    s2: Add libbt-vendor in device.mk
    Bug: 36810308
    Test: build

--- device.mk
+++ device.mk
@@ -140,6 +140,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    libbt-vendor
+
 # Camera
 PRODUCT_PACKAGES += \
     Snap \
