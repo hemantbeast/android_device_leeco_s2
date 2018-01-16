9947b00e5b78552fdbe5faaa5a54069c61bff05b
hemantbeast <hemantbeast@gmail.com>
2018-01-16 18:07:44 +0530

    s2: Add netutils
    Change-Id: Iee153c95fe640df2d98c0aa8db6192aa5f410bb2

--- device.mk
+++ device.mk
@@ -274,6 +274,12 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/vendor/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/vendor/etc/media_codecs_google_video.xml
 
+# Netutils
+PRODUCT_PACKAGES += \
+    netutils-wrapper-1.0 \
+    android.system.net.netd@1.0 \
+    libandroid_net
+
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
